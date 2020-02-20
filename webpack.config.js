7eed4785999c4bdc6cbd0a81eb196d6709ce51e8
oleh <omush@softserveinc.com>
2020-02-20 11:43:56 +0200

    small fix

--- webpack.config.js
+++ webpack.config.js
@@ -19,10 +19,7 @@ module.exports = {
                 test: /\.(js|jsx)$/,
                 exclude: /(node_modules|bower_components)/,
                 use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-env']
-                    }
+                    loader: 'babel-loader'
                 }
             },
             {
