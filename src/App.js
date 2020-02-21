8b933f8012364398ccdb24addc815294929b735e
Oleh <oleh_mushora@ukr.net>
2020-02-21 11:31:56 +0200

    add bootstrap config

--- src/App.js
+++ src/App.js
@@ -2,13 +2,10 @@ import React from 'react';
 import { BrowserRouter } from "react-router-dom";
 import Routes from './router';
 
-import 'bootstrap/dist/css/bootstrap.min.css';
-
 import Header from './components/header';
 
-import './style/index.scss'
-
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './style/index.scss';
 
 function App() {
   return (
--- src/components/header.js
+++ src/components/header.js
@@ -1,14 +1,14 @@
 import React from "react";
 import { Link, NavLink } from 'react-router-dom';
 
-import './header.scss';
+// import './header.scss';
 
 const Header = () => {
     return (
-        <nav className="navbar navbar-expand-lg navbar-light top-bar">
+        <nav className="navbar navbar-expand-sm navbar-dark bg-dark top-bar">
             <div className="container">
                 <Link to="/" className="navbar-brand">
-                    Гарбуз чарт
+                    Гарбуз
                 </Link>
 
 
