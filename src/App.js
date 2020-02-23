1169a20949d38e5aa23fb8e0f34511e67f4bfbbf
skiffua <42241995+skiffua@users.noreply.github.com>
2020-02-23 13:34:05 +0200

    Revert "Add header"

--- src/App.js
+++ src/App.js
@@ -2,17 +2,14 @@ import React from 'react';
 import { BrowserRouter } from "react-router-dom";
 import Routes from './router';
 
-import Header from './components/header';
-
-import './style/index.scss';
-
 function App() {
   return (
     <div className="App">
-        <BrowserRouter>
-            <Header/>
-            <Routes />
-        </BrowserRouter>
+      <header className="App-header">
+      </header>
+      <BrowserRouter>
+        <Routes />
+      </BrowserRouter>
     </div>
   );
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,3 @@
 export Home from './Home';
 export Chart from './Chart';
-export Authentication from './Authentication';
 export About from './About';
