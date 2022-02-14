c1afdef5c5e3b728966069393ce1786280ec8ec4
skiffua <oleh_mushora@ukr.net>
2022-02-14 10:17:58 +0200

    Add rectangle eq

--- src/components/equalizer/equalizer.tsx
+++ src/components/equalizer/equalizer.tsx
@@ -4,16 +4,16 @@ import canvasSketch from 'canvas-sketch';
 import { Howl } from 'howler';
 
 import { Analyser, Player, PlayerInstance } from "../playlist/player";
-import {useEffect, useRef, useState} from "react";
+import { useEffect, useRef, useState } from "react";
 import { eventBus } from "../../event-bus/event-bus";
 import {RefObject} from "react";
-import {Card} from "react-bootstrap";
+import { Card } from "react-bootstrap";
 import './equlizer.scss';
-import {draw} from "./helper";
+import { draw, rectangles } from "./helper";
 
 const Equalizer = (): any => {
     let sound: Howl | null = null;
-    const [dataArray, setData] = useState(new Uint8Array());
+    const [dataArray, setData] = useState(new Uint8Array(128));
     const [context, setContext] = useState(null);
     const styles = {
         border: '0.0625rem solid #9c9c9c',
@@ -27,23 +27,37 @@ const Equalizer = (): any => {
         canvas.current.width = canvas.current.offsetWidth;
         canvas.current.height = canvas.current.offsetHeight;
         console.log('canvas context', context);
-        if (context) {
-            draw(context);
-        }
+        // if (context) {
+        //     draw(context, canvas.current.width, canvas.current.height);
+        // }
     }, [context]);
+
+    useEffect(() => {
+        console.log('dataArray', dataArray);
+        rectangles(context, canvas.current.width, canvas.current.height, dataArray)
+    }, [dataArray]);
 
     useEffect(() => {
         setContext(canvas.current.getContext('2d'));
 
         eventBus.on("onPlaySong", () => {
             sound = Player.getInstance();
+            let intervalId;
 
             if (sound) {
                 sound.on('play', () => {
                     Analyser.createAnalyser(sound);
-                    setInterval(() => {
-                        setData(Uint8Array.from(Analyser.getFrequency())) ;
-                    }, 1000)
+                    intervalId = setInterval(() => {
+                        setData(arr => Uint8Array.from(Analyser.getFrequency())) ;
+                    }, 50)
+                });
+
+                sound.on('pause', () => {
+                    clearInterval(intervalId);
+                });
+
+                sound.on('stop', () => {
+                    clearInterval(intervalId);
                 });
             }
         });
--- src/components/equalizer/helper.ts
+++ src/components/equalizer/helper.ts
@@ -1,4 +1,51 @@
-export function draw(ctx): void {
-    ctx.fillStyle = "green";
-    ctx.fillRect(10, 10, 100, 100);
+export function draw(ctx, width, height): void {
+    // const height: number = ctx.canvas.clientHeight;
+    // const weight: number = ctx.canvas.clientWidth;
+
+    // ctx.fillStyle = "green";
+    // ctx.fillRect(10, 10, 100, 100);
+
+
+    // ctx.arc(100, 75, 50, 0, Math.PI * 2);
+    // ctx.clip();
+    ctx.fillStyle = createGradient(ctx, width / 2, height, width / 2, 0);
+    ctx.fillRect(0, 0, width, height);
+    // ctx.fillStyle = 'white';
+    // ctx.fillRect(0, 0, width, height);
+
+}
+
+function createGradient(ctx, x0, y0, x1, y1) {
+    const gradient = ctx.createLinearGradient(x0, y0, x1, y1);
+
+    gradient.addColorStop(0, 'green');
+    gradient.addColorStop(0.2, 'yellow');
+    gradient.addColorStop(0.6, 'orange');
+    gradient.addColorStop(0.8, 'red');
+    gradient.addColorStop(1, 'black');
+
+    return gradient;
+}
+
+export function rectangles(ctx, width, height, dataArray) {
+    const freq = 128;
+    const border = 1;
+    const recWidth = ~~((width - (freq + 1) * border) / freq);
+    const remainder = (width - freq * border) % freq;
+
+    console.log('rectangles', dataArray);
+
+    if (ctx) {
+        ctx.save();
+        ctx.clearRect(0, 0, width, height);
+        ctx.beginPath();
+        for (let i = 0; i <= freq; i++) {
+            const y = !dataArray[i] ? 0 : dataArray[i];
+            ctx.rect(i * recWidth + i * border,height - y, recWidth, height);
+        }
+        ctx.clip();
+
+        draw(ctx, width, height);
+        ctx.restore();
+    }
 }
