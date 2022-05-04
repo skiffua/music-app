f2aaeeedd2e052d08e37ab5a079fb77adc954247
skiffua <oleh_mushora@ukr.net>
2022-05-04 11:35:50 +0300

    Fix initial pump pics

--- src/components/equalizer/helper.ts
+++ src/components/equalizer/helper.ts
@@ -1,4 +1,4 @@
-import { DIMENSIONS } from './const';
+import {DIMENSIONS} from './const';
 
 const defaultPicsPosition = new Uint8Array(DIMENSIONS.EQUALIZER_FREG / 2).fill(DIMENSIONS.EQUALIZER_HEIGHT);
 const initialPicsPosition = new Uint8Array(DIMENSIONS.EQUALIZER_FREG).fill(DIMENSIONS.EQUALIZER_HEIGHT);
@@ -13,6 +13,8 @@ export class RectangleEqualizer {
     private picJump = 20;
     private picSpeed = 1;
     private largeSize = 900;
+
+    private initialState = true;
 
     constructor(ctx: CanvasRenderingContext2D, width: number, height: number) {
         this.ctx = ctx;
@@ -41,128 +43,58 @@ export class RectangleEqualizer {
     }
 
     initialJumps(): void {
-        console.log('initial');
-        const initialJump = 20;
-        let j = 0;
+        let initialJump = 80;
+        let rightDirection = true;
+        let downDirection = true;
+        let jumpedPic = 0;
         initialPicsPosition.fill(DIMENSIONS.EQUALIZER_HEIGHT - this.picHeight);
 
         const defaultJumping = () => {
             const recWidth = ~~((this.width - (this.freq + 1) * this.colMargin) / this.freq);
-            const halfCanvas = ~~(this.width / 2);
 
             if (this.ctx) {
-                // this.ctx.save();
                 this.ctx.clearRect(0, 0, this.width, this.height);
 
-                initialPicsPosition[j] = initialJump;
-                const speed = 30;
-
-                // y = a (x - h) 2 + k
-
-                console.log('x0, y0', this.colMargin * (j + 1) + recWidth * j, initialPicsPosition[j], this.colMargin * (j + 1) + recWidth * j, initialPicsPosition[j], '********');
+                initialPicsPosition[jumpedPic] = initialJump;
+                const speed = 3;
 
                 for (let k = 0; k <= ~~(this.freq); k++) {
-                    if (k < j) {
-                        const x0 = this.colMargin * (j + 1) + recWidth * j;
-                        const x = this.colMargin * (k + 1) + recWidth * k;
-                        const y = ~~(1/80 *((x - x0) ** 2) + initialPicsPosition[j]);
-                        const step: number = y + this.picHeight < this.height - this.picHeight ? y + this.picHeight : this.height - this.picHeight;
+                    let y;
+                    let prevCount;
 
-                        console.log(x, y, !!(y + this.picHeight), step);
 
-                        initialPicsPosition[k] = step;
+                    if (k < jumpedPic) {
+                        prevCount = jumpedPic - k || 1;
+                        y = rightDirection ? initialPicsPosition[jumpedPic] + prevCount * this.picHeight * speed : initialPicsPosition[jumpedPic] + prevCount * this.picHeight * 5;
                     }
 
-                    // if (k > j) {
-                    //     const step: number = (initialJump + speed + k) < (this.height - this.picHeight) ? initialJump + speed + k : this.height - this.picHeight;
-                    //     initialPicsPosition[k] = step
-                    // }
-
-                    // console.log(initialPicsPosition)
+                    if (k > jumpedPic) {
+                        prevCount = k - jumpedPic || 1;
+                        y = !rightDirection ? initialPicsPosition[jumpedPic] + prevCount * this.picHeight * speed : initialPicsPosition[jumpedPic] + prevCount * this.picHeight * 5;
+                    }
 
 
-                    // if (initialPicsPosition[j - k ] !== undefined && initialPicsPosition[j - k ] + 3 < this.height - this.picHeight) {
-                    //     initialPicsPosition[j - k ] = initialPicsPosition[j - k ] + 3;
-                    // }
-                    //
-                    // if (initialPicsPosition[j + k ] !== undefined && initialPicsPosition[j + k ] + initialJump + 4 + k < this.height - this.picHeight) {
-                    //     initialPicsPosition[j + k ] = initialPicsPosition[j - k ] + initialJump + 4 + k - j;
-                    // }
-
-                    // if ( === j) {
-                    //     initialPicsPosition[k] = initialJump;
-                    //     initialPicsPosition[i - 1] = 70;
-                    //     initialPicsPosition[i + 1] = 80;
-                    // } else {
-                    //     initialPicsPosition[i] = this.height - this.picHeight;
-                    // }
+                    if (k !== jumpedPic) {
+                        initialPicsPosition[k] = y < this.height - this.picHeight ? y : this.height - this.picHeight;
+                    }
                 }
-                // for (let k = 0; k < 6; k++) {
-                //     initialPicsPosition[j] = initialJump;
-                //     // if ( === j) {
-                //     //     initialPicsPosition[k] = initialJump;
-                //     //     initialPicsPosition[i - 1] = 70;
-                //     //     initialPicsPosition[i + 1] = 80;
-                //     // } else {
-                //     //     initialPicsPosition[i] = this.height - this.picHeight;
-                //     // }
-                //
-                // }
-
 
                 for (let i = 0; i < ~~(this.freq); i++) {
-                    // const reqHeight = !dataArray[i] ? 0 : dataArray[i];
-                    // const recY = reqHeight ? ~~((1 - reqHeight / 255) * this.height + this.picHeight) : this.height;
-                    let picY;
-
-                    // if (i === j) {
-                    //     picY = 75;
-                    //     defaultPicsPosition[i] = picY;
-                    //     defaultPicsPosition[i - 1] = picY - 1;
-                    //     defaultPicsPosition[i + 1] = picY - 1;
-                    // } else {
-                    //     defaultPicsPosition[i] = this.height - this.picHeight;
-                    // }
-
-                    // if (recY - this.picHeight <= 0) {
-                    //     picY = 0;
-                    // } else if (recY === this.height) {
-                    //     picY = this.height - this.picHeight;
-                    // } else {
-                    //     picY = recY - this.picJump - this.picHeight;
-                    // }
-
-                    // pics
-
-
-                    // console.log(i, initialPicsPosition[i]);
-                    // if (defaultPicsPosition[i] > recY - this.picHeight) {
-                    //     defaultPicsPosition[i] = picY;
-                    // } else {
-                    //     defaultPicsPosition[i] = defaultPicsPosition[i] < this.height - this.picHeight ?
-                    //         defaultPicsPosition[i] + this.picSpeed : this.height - this.picHeight;
-                    // }
                     this.ctx.fillRect(this.colMargin * (i + 1) + recWidth * i, initialPicsPosition[i], recWidth, this.picHeight);
-                    // this.ctx.fillRect(halfCanvas - this.colMargin * i - recWidth * (i + 1), defaultPicsPosition[i], recWidth, this.picHeight);
                 }
-                // if (i === j) {
-                //         picY = 75;
-                //         defaultPicsPosition[i] = picY;
-                //         defaultPicsPosition[i - 1] = picY - 1;
-                //         defaultPicsPosition[i + 1] = picY - 1;
-                //     } else {
-                //         defaultPicsPosition[i] = this.height - this.picHeight;
-                //     }
+                if ((jumpedPic > ~~(this.freq * 0.8)) && rightDirection) {
+                    rightDirection = !rightDirection;
+                } else if ((jumpedPic < ~~(this.freq * 0.2)) && !rightDirection) {
+                    rightDirection = !rightDirection;
+                }
 
-                // defaultPicsPosition[j] = 75;
-                // defaultPicsPosition[j - 1] = 70;
-                // defaultPicsPosition[j + 1] = 80;
-                if (j > ~~(this.freq)) {
-                    j = 0;
-                } else {
-                    j++;
+                jumpedPic = rightDirection ? jumpedPic + 1 : jumpedPic - 1;
+                initialJump = downDirection ? initialJump + 2 : initialJump - 2;
+                if (initialJump > this.height - this.picHeight - 5) {
+                    downDirection = !downDirection;
+                } else if (initialJump < 80) {
+                    downDirection = !downDirection;
                 }
-                // this.ctx.restore();
             }
         };
 
@@ -172,7 +104,7 @@ export class RectangleEqualizer {
                     defaultJumping();
                 }
             });
-        }, 150);
+        }, 200);
     }
 
     rectangles(dataArray) {
