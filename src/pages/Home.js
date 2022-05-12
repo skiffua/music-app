8a54a7f87ca2355acc691fec0ffc2797029e63a3
skiffua <oleh_mushora@ukr.net>
2022-05-12 12:27:50 +0300

    Add fileloader

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,30 @@
 import React from "react";
 
+import mainBackGroundWebp1440 from '../assets/images/background-music-1440.webp';
+import mainBackGroundWebp700 from '../assets/images/background-music-1440.webp';
+import mainBackGroundWebp200 from '../assets/images/background-music-1440.webp';
+import mainBackGround1440 from '../assets/images/background-music-1440.png';
+import mainBackGround700 from '../assets/images/background-music-1440.png';
+import mainBackGround200 from '../assets/images/background-music-1440.png';
+
+import './home.scss';
+
 const Home = () => {
-    return <div>Домівка</div>
+    return (
+        <div className="homepage">
+            <div className="homepage-background">
+                <picture className="homepage-background">
+                    <source srcSet={`${mainBackGroundWebp1440} 1440w, ${mainBackGroundWebp700} 700w" sizes="700vw", ${mainBackGroundWebp200} 700w" sizes="200vw"`}
+                            type="image/webp" />
+                    <img className="homepage-background-img"
+                         alt="music background cover" src={mainBackGround1440}
+                         srcSet={`${mainBackGround1440} 1440w, ${mainBackGround700} 700w" sizes="700vw", ${mainBackGround200} 700w" sizes="200vw"`}
+                         sizes="100vw"/>
+
+                </picture>
+            </div>
+        </div>
+    )
 };
 
 export default Home
